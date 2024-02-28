29ea53661c16c384bf6142e89801498646fe7d3f
Rajeshwari <88048602+RajeshwariBhandari@users.noreply.github.com>
2024-02-28 09:41:08 +0530

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -3,17 +3,15 @@ const UserSchema = new mongoose.Schema({
     name:{
         type: String, 
         required: [true, 'Please provide name'], 
-        // minlength: 3, 
-        // maxlength: 20, 
         trim: true, 
     },
     email:{
         type: String, 
         required: [true, 'Please provide email'],
-        // validate:{
-        //     validator: validator.isEmail,
-        //     message:'Please provide valid email'
-        // },
+        validate:{
+            validator: validator.isEmail,
+            message:'Please provide valid email'
+        },
         unique: true, 
     },
     age:{
